feffda84aef2640eb7eed5209e3130fac8b06786
영원휘이 <????????>
2021-05-13 11:30:13 +0000

    ?

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -22,18 +22,7 @@ def search_youtube():
     keyword = request.args.get('keyword')
     if not keyword:
         return "missing keyword"
-    return jsonify(utils.search_youtube(keyword))
+    return utils.search_youtube(keyword)
 
-youtube_api_key = 'AIzaSyC-5mfLh9EWPdHNOv25ngb2UfNYh6eZ4Uo'
-def get_video_key(query):
-    args={'part': 'snippet', 'maxResults': 1, 'q': query, 'type': 'video', 'key': youtube_api_key}
-    r = requests.get('https://youtube.googleapis.com/youtube/v3/search',params=args)
-    js=json.loads(r.text)
-    return js['items'][0]['id']['videoId']
-
-
-print(get_video_key("광안리 불꽃축제"))
-
-
-app.run('0.0.0.0', 5000)
+#app.run('0.0.0.0', 5000)
 
--- BackEnd/utils.py
+++ BackEnd/utils.py
@@ -21,6 +21,14 @@ def parseTour(location):
             })
     return result
 
+# 유튜브 api 이용
+youtube_api_key = 'AIzaSyC-5mfLh9EWPdHNOv25ngb2UfNYh6eZ4Uo'
+def get_video_key(query):
+    args={'part': 'snippet', 'maxResults': 1, 'q': query, 'type': 'video', 'key': youtube_api_key}
+    r = requests.get('https://youtube.googleapis.com/youtube/v3/search',params=args)
+    js=json.loads(r.text)
+    return js['items'][0]['id']['videoId']
+
 # 임시 유튜브 스크래퍼
 def search_youtube(keyword):
     # 동영상 만 검색하는 필터
