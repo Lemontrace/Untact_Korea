4b0d5cf6b7f5fe29af898bc9f7c2e96117def29f
algosketch <sjy_9484@naver.com>
2021-05-13 18:24:18 +0900

    add local map border

--- FrontEnd/untact_korea/src/components/Content.js
+++ FrontEnd/untact_korea/src/components/Content.js
@@ -5,7 +5,7 @@ function Content() {
         <div className="full-map">
 
         </div>
-        <div className="local-map">
+        <div className="description">
 
         </div>
       </div>
@@ -13,6 +13,12 @@ function Content() {
       <div className="plane-animation">
         <img className={"plane"} src="./plane.svg" alt="plane"/>
         <img className={"flying-route"} src="./flying-route.svg" alt="flying line"/>
+      </div>
+
+      <div className="local-map-border">
+        <div className="local-map">
+
+        </div>
       </div>
     </main>
   )
