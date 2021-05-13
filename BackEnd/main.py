888ab30e4a963e33812817db5ff9c6e519cce44b
영원휘이 <????????>
2021-05-13 08:01:42 +0000

    /get_data test

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,13 @@
 from flask import *
 
-app = Flask(__name__)
 
+app = Flask(__name__,static_folder='data')
+
+@app.route('/get_data')
+def get_data():
+    data_type = request.args.get('data_type')
+    location = request.args.get('location')
+    
+    if (data_type=='festival'):
+        return app.send_static_file('%s.json' % location)
+        
