409d177a7a63dfaf403468dffdb714bdd17ef7ca
영원휘이 <????????>
2021-05-13 09:48:02 +0000

    ??

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,31 +1,30 @@
 from flask import Flask, request
 import requests
+import json
 
 app = Flask(__name__, static_folder='data')
 
 
-@app.route('/get_data')
-def get_data():
-    data_type = request.args.get('type')
+@app.route('/getFestivals')
+def get_festivals():
     location = request.args.get('location')
+    return app.send_static_file('festival/%s.json' % location)
 
-    if data_type == 'festival':
-        return app.send_static_file('festival/%s.json' % location)
-    if data_type == 'tour':
-        pass
-        # code
+@app.route('/getTours')
+def get_tours():
+    pass
+    #TODO
 
-
-
-youtube_api_key = 'AIzaSyDbMPQCMvNP9FM4gP7Of36ki2iuwSQYRlo'
-def make_youtube_api_request(query):
+youtube_api_key = 'AIzaSyC-5mfLh9EWPdHNOv25ngb2UfNYh6eZ4Uo'
+def get_video_key(query):
     args={'part': 'snippet', 'maxResults': 1, 'q': query, 'type': 'video', 'key': youtube_api_key}
     r = requests.get('https://youtube.googleapis.com/youtube/v3/search',params=args)
-    return r.text
-    
+    js=json.loads(r.text)
+    return js['items'][0]['id']['videoId']
 
 
-print(make_youtube_api_request("광안리 불꽃축제"))
+print(get_video_key("광안리 불꽃축제"))
 
 
 #app.run('0.0.0.0', 5000)
+
