a9ec698e72afe60a30e84ceb9f0b869fa8ee931c
재영 정 <jjy600901@snu.ac.kr>
2021-05-13 22:02:06 +0900

    스크래퍼 예외 상황 처리

--- BackEnd/utils.py
+++ BackEnd/utils.py
@@ -4,6 +4,7 @@ import requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 tourFile = os.path.join(basedir, 'data/tour.json')
+failLog = os.path.join(basedir, 'log/fail.log')
 
 def parseTour(location):
     result = []
@@ -24,11 +25,27 @@ def parseTour(location):
 # 임시 유튜브 스크래퍼
 def search_youtube(keyword):
     # 동영상 만 검색하는 필터
-    filter = "&sp=EgIQAQ%253D%253D"
-    html = requests.get('https://www.youtube.com/results?search_query=' + keyword + filter).text
+    threshold = 5
+    try:
+        while threshold:
+            request_url = 'https://www.youtube.com/results?search_query=' + keyword
+            html = requests.get(request_url).text
 
-    index = html.find('"watchEndpoint":{"videoId":"')
+            index = html.find('"watchEndpoint":{"videoId":"')
 
-    target = html[index: index + 100]
-    videoId = target.split('"')[5]
-    return videoId
+            # 최대 threshold 번 만큼 재시도함
+            if index == -1:
+                threshold -= 1
+                continue
+
+            target = html[index: index + 100]
+            video_id = (target.split('"')[5])
+            return video_id
+
+        # 이심원충신정려현판 은 검색결과가 없기 때문에, 무조건 여기로 옴.
+        raise(Exception("Search Failed"))
+    except:
+        with open(failLog, "a") as f:
+            f.write(keyword + "\n")
+    # Fake videoId
+    return "X" * 11
